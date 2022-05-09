33f47061d4768831100c8a268e272be3c470647b
d0641798 <tsai100038@gmail.com>
2022-05-09 18:54:59 +0800

    Create Point.java
    class point

---
+++ Point.java
@@ -0,0 +1,21 @@
+
+public class Point {
+	private int x,y;
+	public void setX(int x){
+	 this.x = x;
+	 }
+	public void setY(int y){
+	 this.y = y;
+	}
+	public int getX() {
+	return x;
+	}
+	public int getY() {
+	return y;
+	}
+	@Override
+	public String toString() {
+	return "Point [x=" + x + ", y=" + y + "]";
+	}
+
+}
